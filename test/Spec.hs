c9bb440ce91746e85dc344405b53ad5e3e790323
Nick <dev@nsenger.com>
2019-10-27 23:43:02 -0700

    chore: cleanup

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,6 @@ module Main where
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
-
 import           Lib
 
 instance Arbitrary a => Arbitrary (Turducken a) where
@@ -16,16 +15,16 @@ instance Arbitrary a => Arbitrary (Turducken a) where
 instance Eq a => EqProp (Turducken a) where
   (=-=) = eq
 
-trigger = undefined :: Turducken (String, String, Int)
+semiTrigger = undefined :: (Turducken (String, String, Int), Int)
+monoTrigger = undefined :: (Turducken (String, String, Int), String)
+funcTrigger = undefined :: Turducken (String, String, Int)
 foldTrigger = undefined :: Turducken (Int, Int, [Int], Int, Int)
 traverseTrigger = undefined :: Turducken (Int, Int, [Int])
 
 main :: IO ()
 main = do
-  quickBatch $ semigroup (undefined :: (Turducken (String, String, Int), Int))
-  quickBatch $ monoid (undefined :: (Turducken (String, String, Int), String))
-  quickBatch $ functor trigger
+  quickBatch $ semigroup semiTrigger
+  quickBatch $ monoid monoTrigger
+  quickBatch $ functor funcTrigger
   quickBatch $ foldable foldTrigger
   quickBatch $ traversable traverseTrigger
-  -- quickBatch $ applicative turduckenTrigger
-  -- quickBatch $ monad turduckenTrigger
