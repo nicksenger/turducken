f0b520448f8def47120e2060de96b2958d349035
Nick <dev@nsenger.com>
2019-10-27 22:15:34 -0700

    feat: implement basic typeclass instances

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,48 @@
 module Lib where
 
 data Turducken a =
-    Empty |
-    Leaf a [Turducken a] |
-    Node a [Turducken a] (Turducken a) (Turducken a)
+  Empty |
+  Node a (Turducken a) (Turducken a) |
+  Container [Turducken a] (Turducken a) (Turducken a)
+  deriving (Eq, Show)
+
+instance Semigroup (Turducken a) where
+  Empty <> Empty = Empty
+  Empty <> t     = t
+  t     <> Empty = t
+  (Node x left1 right1) <> (Node y left2 right2) =
+    Container [(Node x left1 right1), (Node y left2 right2)] Empty Empty
+  (Node x left1 right1) <> (Container sub left2 right2) =
+    Container ((Node x left1 right1) : sub) left2 right2
+  (Container sub left1 right1) <> (Node x left2 right2) =
+    Container (sub ++ [(Node x left2 right2)]) left1 right1
+  (Container sub1 left1 right1) <> (Container sub2 left2 right2) =
+    Container (sub1 ++ sub2) (left1 <> left2) (right1 <> right2)
+
+instance Monoid (Turducken a) where
+  mempty = Empty
+
+instance Functor Turducken where
+  fmap _ Empty               = Empty
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+  fmap f (Container sub left right) =
+    Container ((fmap . fmap) f sub) (fmap f left) (fmap f right)
+
+instance Foldable Turducken where
+  foldMap _ Empty = mempty
+  foldMap f (Node x left right) =
+    (f x) <> (foldMap f left) <> (foldMap f right)
+  foldMap f (Container xs left right) =
+    (foldr ((<>) . (foldMap f)) mempty xs)
+      <> (foldMap f left)
+      <> (foldMap f right)
+
+instance Traversable Turducken where
+  traverse _ Empty = pure Empty
+  traverse f (Node x left right) =
+    Node <$> (f x) <*> (traverse f left) <*> (traverse f right)
+  traverse f (Container xs left right) =
+    Container
+      <$> ((traverse . traverse) f xs)
+      <*> (traverse f left)
+      <*> (traverse f right)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,31 @@
+module Main where
+
+import           Test.QuickCheck
+import           Test.QuickCheck.Checkers
+import           Test.QuickCheck.Classes
+
+import           Lib
+
+instance Arbitrary a => Arbitrary (Turducken a) where
+  arbitrary = frequency
+    [ (100, pure Empty)
+    , (10, Node <$> arbitrary <*> arbitrary <*> arbitrary)
+    , (1, Container <$> arbitrary <*> arbitrary <*> arbitrary)
+    ]
+
+instance Eq a => EqProp (Turducken a) where
+  (=-=) = eq
+
+trigger = undefined :: Turducken (String, String, Int)
+foldTrigger = undefined :: Turducken (Int, Int, [Int], Int, Int)
+traverseTrigger = undefined :: Turducken (Int, Int, [Int])
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickBatch $ semigroup (undefined :: (Turducken (String, String, Int), Int))
+  quickBatch $ monoid (undefined :: (Turducken (String, String, Int), String))
+  quickBatch $ functor trigger
+  quickBatch $ foldable foldTrigger
+  quickBatch $ traversable traverseTrigger
+  -- quickBatch $ applicative turduckenTrigger
+  -- quickBatch $ monad turduckenTrigger
