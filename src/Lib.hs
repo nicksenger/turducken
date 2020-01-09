29177502800daa17029fae3df13e022cf8bb14af
Nick <dev@nsenger.com>
2020-01-09 14:46:08 -0800

    chore: module stuff

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,8 +13,8 @@ instance (Monoid a) => Semigroup (Turducken a b) where
   t     <> Empty = t
   (Node x l1 r1) <> (Node y l2 r2) =
     Group mempty [(Node x l1 r1), (Node y l2 r2)] Empty Empty
-  (Node  x  l1 r1) <> (Group a ts l2 r2) = Group a ((Node x l1 r1) : ts) l2 r2
-  (Group a ts l1 r1) <> (Node  x  l2 r2) = Group a (ts ++ [(Node x l2 r2)]) l1 r1
+  (Node x l1 r1) <> (Group a ts l2 r2) = Group a ((Node x l1 r1) : ts) l2 r2
+  (Group a ts l1 r1) <> (Node x l2 r2) = Group a (ts ++ [(Node x l2 r2)]) l1 r1
   (Group a1 ts1 l1 r1) <> (Group a2 ts2 l2 r2) =
     Group (a1 <> a2) (ts1 ++ ts2) (l1 <> l2) (r1 <> r2)
 
@@ -22,8 +22,8 @@ instance (Monoid a) => Monoid (Turducken a b) where
   mempty = Empty
 
 instance Functor (Turducken a) where
-  fmap _ Empty          = Empty
-  fmap f (Node  x  l r) = Node (f x) (fmap f l) (fmap f r)
+  fmap _ Empty            = Empty
+  fmap f (Node x l r    ) = Node (f x) (fmap f l) (fmap f r)
   fmap f (Group a ts l r) = Group a ((fmap . fmap) f ts) (fmap f l) (fmap f r)
 
 instance (Monoid a) => Applicative (Turducken a) where
@@ -34,8 +34,8 @@ instance (Monoid a) => Monad (Turducken a) where
   return = pure
   Empty          >>= f = Empty
   (Node x l1 r1) >>= f = case (f x) of
-    Empty          -> Empty
-    Node  y  l2 r2 -> Node y (l2 <> (l1 >>= f)) (r2 <> (r1 >>= f))
+    Empty            -> Empty
+    Node y l2 r2     -> Node y (l2 <> (l1 >>= f)) (r2 <> (r1 >>= f))
     Group a ts l2 r2 -> Group a ts (l2 <> (l1 >>= f)) (r2 <> (r1 >>= f))
   (Group a ts l r) >>= f = Group a (fmap (>>= f) ts) (l >>= f) (r >>= f)
 
@@ -50,4 +50,7 @@ instance Traversable (Turducken a) where
   traverse f (Node x l r) =
     Node <$> (f x) <*> (traverse f l) <*> (traverse f r)
   traverse f (Group a ts l r) =
-    Group a <$> ((traverse . traverse) f ts) <*> (traverse f l) <*> (traverse f r)
+    Group a
+      <$> ((traverse . traverse) f ts)
+      <*> (traverse f l)
+      <*> (traverse f r)
---
+++ src/Turducken.hs
@@ -0,0 +1,9 @@
+module Turducken
+  ( Turducken(Empty, Group, Node)
+  , drawTurducken
+  )
+where
+
+import           Lib                            ( Turducken(Empty, Group, Node)
+                                                )
+import           Draw                           ( drawTurducken )
