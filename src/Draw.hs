285bba1abd64966e09f869b6626cf062eb6a9458
Nick <dev@nsenger.com>
2020-01-09 11:17:26 -0800

    feat: refactor to have group values

--- src/Draw.hs
+++ src/Draw.hs
@@ -5,29 +5,29 @@ where
 
 import           Lib
 
-drawTurducken :: Turducken String -> String
+drawTurducken :: (Show a, Show b) => Turducken a b -> String
 drawTurducken = unlines . draw
 
-draw :: Turducken String -> [String]
+draw :: (Show a, Show b) => Turducken a b -> [String]
 draw Empty        = []
-draw (Node x l r) = x : (drawChildTurduckens (filter (not . isEmpty) [r, l]))
-draw (Group ts l r) =
+draw (Node x l r) = (show x) : (drawChildTurduckens (filter (not . isEmpty) [r, l]))
+draw (Group a ts l r) =
   [drawBorder ts '`']
     ++ (drawSubTurduckens ts)
     ++ [drawBorder ts '_']
     ++ (drawChildTurduckens (filter (not . isEmpty) [r, l]))
 
-drawBorder :: [Turducken String] -> Char -> String
+drawBorder :: (Show a, Show b) => [Turducken a b] -> Char -> String
 drawBorder ts c = "|" ++ (rep (maxGroupLength ts) c) ++ "|"
   where rep n x = [ x | _ <- [1 .. n] ]
 
-drawSubTurduckens :: [Turducken String] -> [String]
+drawSubTurduckens :: (Show a, Show b) => [Turducken a b] -> [String]
 drawSubTurduckens ts = case ts of
   []       -> []
   [t     ] -> "|" : shift "|  " "|  " (draw t)
   (t : ts) -> "|" : shift "|  " "|  " (draw t) ++ drawSubTurduckens ts
 
-drawChildTurduckens :: [Turducken String] -> [String]
+drawChildTurduckens :: (Show a, Show b) => [Turducken a b] -> [String]
 drawChildTurduckens ts = case ts of
   []       -> []
   [t     ] -> "|" : shift "`- " "   " (draw t)
@@ -36,28 +36,28 @@ drawChildTurduckens ts = case ts of
 shift :: [a] -> [a] -> [[a]] -> [[a]]
 shift first other = zipWith (++) (first : repeat other)
 
-isEmpty :: Turducken a -> Bool
+isEmpty :: Turducken a b -> Bool
 isEmpty Empty = True
 isEmpty _     = False
 
-height :: Turducken a -> Int
+height :: Turducken a b -> Int
 height Empty          = 0
 height (Node  _  l r) = 1 + max (height l) (height r)
-height (Group ts l r) = 2 + maximum (fmap height (l : r : ts))
+height (Group a ts l r) = 2 + maximum (fmap height (l : r : ts))
 
-withHeight :: Turducken a -> Turducken (Int, a)
+withHeight :: Turducken a b -> Turducken a (Int, b)
 withHeight Empty          = Empty
 withHeight n@(Node x l r) = Node (height n, x) (withHeight l) (withHeight r)
-withHeight (Group ts l r) =
-  Group (fmap withHeight ts) (withHeight l) (withHeight r)
+withHeight (Group a ts l r) =
+  Group a (fmap withHeight ts) (withHeight l) (withHeight r)
 
-maxLength :: Turducken String -> Int
+maxLength :: (Show a, Show b) => Turducken a b -> Int
 maxLength t = maximum $ fmap go (withHeight t)
  where
-  go (n, s) = ((totalHeight - n) * 3) + (length s) + 4
+  go (n, s) = ((totalHeight - n) * 3) + (length (show s)) + 4
   totalHeight = maximum $ fmap justHeight wh
   justHeight (n, s) = n
   wh = withHeight t
 
-maxGroupLength :: [Turducken String] -> Int
+maxGroupLength :: (Show a, Show b) => [Turducken a b] -> Int
 maxGroupLength ts = maximum $ fmap maxLength ts
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,52 +2,52 @@ module Lib where
 
 import           Control.Monad                  ( ap )
 
-data Turducken a =
+data Turducken a b =
   Empty |
-  Node a (Turducken a) (Turducken a) |
-  Group [Turducken a] (Turducken a) (Turducken a)
+  Node b (Turducken a b) (Turducken a b) |
+  Group a [Turducken a b] (Turducken a b) (Turducken a b)
   deriving (Eq, Show)
 
-instance Semigroup (Turducken a) where
+instance (Monoid a) => Semigroup (Turducken a b) where
   Empty <> t     = t
   t     <> Empty = t
   (Node x l1 r1) <> (Node y l2 r2) =
-    Group [(Node x l1 r1), (Node y l2 r2)] Empty Empty
-  (Node  x  l1 r1) <> (Group ts l2 r2) = Group ((Node x l1 r1) : ts) l2 r2
-  (Group ts l1 r1) <> (Node  x  l2 r2) = Group (ts ++ [(Node x l2 r2)]) l1 r1
-  (Group ts1 l1 r1) <> (Group ts2 l2 r2) =
-    Group (ts1 ++ ts2) (l1 <> l2) (r1 <> r2)
+    Group mempty [(Node x l1 r1), (Node y l2 r2)] Empty Empty
+  (Node  x  l1 r1) <> (Group a ts l2 r2) = Group a ((Node x l1 r1) : ts) l2 r2
+  (Group a ts l1 r1) <> (Node  x  l2 r2) = Group a (ts ++ [(Node x l2 r2)]) l1 r1
+  (Group a1 ts1 l1 r1) <> (Group a2 ts2 l2 r2) =
+    Group (a1 <> a2) (ts1 ++ ts2) (l1 <> l2) (r1 <> r2)
 
-instance Monoid (Turducken a) where
+instance (Monoid a) => Monoid (Turducken a b) where
   mempty = Empty
 
-instance Functor Turducken where
+instance Functor (Turducken a) where
   fmap _ Empty          = Empty
   fmap f (Node  x  l r) = Node (f x) (fmap f l) (fmap f r)
-  fmap f (Group ts l r) = Group ((fmap . fmap) f ts) (fmap f l) (fmap f r)
+  fmap f (Group a ts l r) = Group a ((fmap . fmap) f ts) (fmap f l) (fmap f r)
 
-instance Applicative Turducken where
+instance (Monoid a) => Applicative (Turducken a) where
   pure x = Node x Empty Empty
   (<*>) = ap
 
-instance Monad Turducken where
+instance (Monoid a) => Monad (Turducken a) where
   return = pure
   Empty          >>= f = Empty
   (Node x l1 r1) >>= f = case (f x) of
     Empty          -> Empty
     Node  y  l2 r2 -> Node y (l2 <> (l1 >>= f)) (r2 <> (r1 >>= f))
-    Group ts l2 r2 -> Group ts (l2 <> (l1 >>= f)) (r2 <> (r1 >>= f))
-  (Group ts l r) >>= f = Group (fmap (>>= f) ts) (l >>= f) (r >>= f)
+    Group a ts l2 r2 -> Group a ts (l2 <> (l1 >>= f)) (r2 <> (r1 >>= f))
+  (Group a ts l r) >>= f = Group a (fmap (>>= f) ts) (l >>= f) (r >>= f)
 
-instance Foldable Turducken where
+instance Foldable (Turducken a) where
   foldMap _ Empty        = mempty
   foldMap f (Node x l r) = (f x) <> (foldMap f l) <> (foldMap f r)
-  foldMap f (Group ts l r) =
+  foldMap f (Group a ts l r) =
     (foldr ((<>) . (foldMap f)) mempty ts) <> (foldMap f l) <> (foldMap f r)
 
-instance Traversable Turducken where
+instance Traversable (Turducken a) where
   traverse _ Empty = pure Empty
   traverse f (Node x l r) =
     Node <$> (f x) <*> (traverse f l) <*> (traverse f r)
-  traverse f (Group ts l r) =
-    Group <$> ((traverse . traverse) f ts) <*> (traverse f l) <*> (traverse f r)
+  traverse f (Group a ts l r) =
+    Group a <$> ((traverse . traverse) f ts) <*> (traverse f l) <*> (traverse f r)
