17ff48298bb56c9abadd87fb86fbed22a23ae4a8
Nick Senger <nicholas.senger@synthego.com>
2020-02-05 20:46:39 -0800

    feat: improve drawing algorithm

--- src/Draw.hs
+++ src/Draw.hs
@@ -20,14 +20,15 @@ draw g@(Group a ts l r) =
     ++ (drawChildTurduckens (filter (not . isEmpty) [r, l]))
 
 drawBorder :: (Show a, Show b) => Bool -> Turducken a b -> Char -> String
-drawBorder b g@(Group a _ _ _) c = "|" ++ (rep (maxLength g) c) ++ "|"
+drawBorder showTitle g@(Group a _ _ _) c =
+  "|" ++ (flank (maxSubHeight g) c) ++ "|"
  where
   halfLength n = (n - (length (show a))) `div` 2
-  flank n x = [ x | _ <- [1 .. (halfLength n)] ]
-  flanked s x = case b of
+  pad n x = [ x | _ <- [1 .. (halfLength n)] ]
+  padded s x = case showTitle of
     True  -> s
     False -> [ x | _ <- [1 .. (length s)] ]
-  rep n x = (flank n x) ++ (flanked (show a) x) ++ (flank n x)
+  flank n x = (pad n x) ++ (padded (show a) x) ++ (pad n x)
 
 drawSubTurduckens :: (Show a, Show b) => [Turducken a b] -> [String]
 drawSubTurduckens ts = case ts of
@@ -48,33 +49,26 @@ isEmpty :: Turducken a b -> Bool
 isEmpty Empty = True
 isEmpty _     = False
 
-height :: (Show a, Show b) => Turducken a b -> Int
-height Empty            = 0
-height (Node _ l r    ) = 2 + max (height l) (height r)
-height (Group a ts l r) = 2 + maximum (fmap height (l : r : ts))
-
-withHeight :: (Show a, Show b) => Turducken a b -> Turducken a (Int, String)
-withHeight Empty = Empty
-withHeight n@(Node x l r) =
-  Node (height n, show x) (withHeight l) (withHeight r)
-withHeight (Group a ts l r) =
-  Group a (fmap withHeight ts) (withHeight l) (withHeight r)
-
-allNodeHeights :: (Show a, Show b) => Turducken a b -> [(Int, String)]
-allNodeHeights Empty = []
-allNodeHeights n@(Node x l r) =
-  (height n, show x) : allNodeHeights l ++ allNodeHeights r
-allNodeHeights g@(Group y ts l r) =
-  (height g, show y)
-    : (  (foldr ((<>) . allNodeHeights) [] ts)
-      ++ (allNodeHeights l)
-      ++ (allNodeHeights r)
-      )
-
-maxLength :: (Show a, Show b) => Turducken a b -> Int
-maxLength t = maximum $ fmap go (allNodeHeights t)
+height :: (Show a, Show b) => Turducken a b -> (Int, Int)
+height Empty = (0, 0)
+height (Node x l r) =
+  (maximum [length $ show x, fst hl, fst hr], 4 + (max (snd hl) (snd hr)))
  where
-  go (n, s) = ((totalHeight - n) * 3) + (length s) + 4
-  totalHeight = maximum $ fmap justHeight wh
-  justHeight (n, s) = n
-  wh = withHeight t
+  hl = height l
+  hr = height r
+height g@(Group a ts l r) = case addHeight hg > addHeight hl of
+  True -> case addHeight hg > addHeight hr of
+    True  -> hg
+    False -> hr
+  False -> case addHeight hl > addHeight hr of
+    True  -> hl
+    False -> hr
+ where
+  hg        = (0, length (drawBorder False g '-'))
+  hl        = height l
+  hr        = height r
+  addHeight = \(x, y) -> x + y
+
+maxSubHeight :: (Show a, Show b) => Turducken a b -> Int
+maxSubHeight g@(Group _ ts _ _) =
+  maximum $ fmap (\(x, y) -> x + y) $ fmap height ts
